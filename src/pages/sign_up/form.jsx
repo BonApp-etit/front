7a690a4a985a18cfe490444a8e3eeaf26fb7d268
Isaac Vazquez <ivzc07@gmail.com>
2024-10-04 01:12:11 -0600

    add responsivness

--- src/pages/sign_up/form.jsx
+++ src/pages/sign_up/form.jsx
@@ -2,7 +2,7 @@ import Input from '../../components/common_components/StyledInput'
 import { Button } from "@mui/material";
 export default function form(){
     return(
-        <main className="flex flex-col font-poppins ml-[100px]">
+        <main className="flex flex-col  font-poppins ml-[100px] md:mt-8">
             <h3 className="text-[28px] font-semibold font-poppins">Registrate</h3>
             <p className="text-[12px]">Registrate y dile que si a mil clientes satisfechos</p>
             <h2 classhName="text-[16px]         font-light">Nombre Completo</h2>
