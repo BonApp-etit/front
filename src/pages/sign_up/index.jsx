022d4e09a3fc958ec1848de38b8e54c0747aee88
Isaac Vazquez <ivzc07@gmail.com>
2024-10-04 08:55:23 -0600

    fix responsive errors

--- src/pages/sign_up/index.jsx
+++ src/pages/sign_up/index.jsx
@@ -2,9 +2,9 @@ import Form from "./form"
 export default function signup(){
     return(
         <>  
-            <main className="mx-120 flex flex-col lg:flex-row items-center justify-center h-screen">
+            <main className="mx-120 flex flex-col-reverse lg:flex-row items-center justify-center h-screen">
                 <img src="/assets/signupmenu/signupimage.svg" alt="img" className="w-[500px] h-[500px]"/>
-                <Form className="mr-[100px]"/>
+                <Form className=""/>
             </main>
         </>
     )
