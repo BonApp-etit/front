0a64867d903d56429644f4e8653921006a14f43f
Leonardo Meza Pantoja <leonardomp2200@gmail.com>
2024-10-08 15:50:02 -0600

    desktop first finish responsive design

--- src/components/landing/FirstSection.jsx
+++ src/components/landing/FirstSection.jsx
@@ -1,55 +1,92 @@
+import Image from "next/image";
+import { Button } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 export default function First() {
+  const theme = useTheme();
   return (
-    <div className="bg-[#FFF1ED]"> {/* Contenedor exterior que extiende el fondo */}
-      <div className="flex flex-col justify-between lg:flex-row items-center p-11 md:p-12   space-y-6 md:space-y-0 font-sans mx-[120px]">
-        
-        {/* Left Side: Text and Button */}
-        <div className="text-center md:text-left space-y-4 md:w-1/2 ">
-          <h1 className="text-[64px] font-bold text-gray-800 font-roboto">
-            Ordena fácilmente, <br /> disfruta rápidamente
-          </h1>
-          <p className="text-gray-600 text-[24px] font-roboto">
-            Ordena lo que te gusta en pocos pasos y disfruta de <br />tu comida sin esperar.
-          </p>
-          <button className="bg-[#FF441F] hover:bg-orange-600 text-white py-2 px-4 rounded-full font-roboto">
-            ¡Solicita una demo!
-          </button>
-          <div className="flex items-center space-x-2 pt-4">
-            <div className="flex -space-x-2 md:mr-10 sm:mr-0">
-              <img
+    <section className=" lg:bg-[#FFF1ED] lg:px-16 lg:py-[98px] lg:flex lg:justify-center  lg:w-full md:bg-white  px-[90px] ">
+      {/* Contenedor exterior que extiende el fondo */}
+
+      <div className="lg:flex lg:justify-between md:flex md:flex-col md:items-center ">
+        <div>
+          {/**Title and subtittle */}
+          <div className=" lg:mb-12 md:text-center md:mb-9 ">
+            <h1 className="lg:text-[64px] lg:font-bold lg:text-black lg:font-roboto md:text-[50px] md:font-bold md:font-poppins  ">
+              Ordena fácilmente, disfruta rápidamente
+            </h1>
+            <p className="lg:text-black lg:text-2xl lg:font-normal lg:font-roboto md:text-xl md:font-normal md:font-roboto   ">
+              Ordena lo que te gusta en pocos pasos y disfruta de tu comida sin
+              esperar.
+            </p>
+          </div>
+          {/** Button and recomendations */}
+
+          <div className="mb-7 md:flex md:justify-center py-">
+            <Button
+              variant="contained"
+              sx={{
+                [theme.breakpoints.down("lg")]: {
+                  borderRadius: "45px",
+                  py: "18px",
+                  px: "32px",
+                  fontSize: "1rem",
+                  fontWeight: "700",
+                },
+
+                [theme.breakpoints.down("md")]: {
+                  py: "12px", // Cambios para tablet (menor a 768px)
+                  px: "24px",
+                },
+                [theme.breakpoints.down("sm")]: {
+                  py: "8px", // Cambios para móviles pequeños (menor a 640px)
+                  px: "16px",
+                },
+              }}
+            >
+              ¡Solicita una demo!
+            </Button>
+          </div>
+
+          <div className="flex justify-start">
+            <div className="flex -space-x-6 ">
+              <Image
                 src="/assets/pp1.svg"
                 alt="User1"
-                className="w-8 h-8 rounded-full border-2 border-white"
+                width={63}
+                height={63}
+                className=" rounded-full border-2 border-white"
               />
-              <img
+              <Image
                 src="/assets/pp2.svg"
                 alt="User2"
-                className="w-8 h-8 rounded-full border-2 border-white"
+                width={63}
+                height={63}
+                className="rounded-full border-2 border-white"
               />
-              <img
+              <Image
                 src="/assets/pp3.svg"
                 alt="User3"
-                className="w-8 h-8 rounded-full border-2 border-white"
+                width={63}
+                height={63}
+                className="rounded-full border-2 border-white"
               />
             </div>
-            <div className="text-sm text-gray-600 flex-col">
-              <span className="font-semibold">Nuestros usuarios felices</span>
-              <span>⭐ 4.8 (450+ reviews)</span>
+            <div className=" flex flex-col ext-xl font-roboto items-center ">
+              <span className="font-bold  text-black">
+                Nuestros usuarios felices
+              </span>
+              <span className="text-black/50  font-normal ">
+                ⭐ 4.8 (450+ reviews)
+              </span>
             </div>
           </div>
         </div>
-  
+
         {/* Right Side: Chef Image */}
         <div className="mt-[10px] lg:mt-0 md:mt-10">
-          <img
-            src="/assets/first.svg" // Cambia esta ruta con tu imagen
-            alt="Chef"
-            className="rounded-lg md:mt-10"
-          />
+          <Image width={550} height={440} src="/assets/first.svg" alt="Chef" />
         </div>
-      
       </div>
-    </div>
+    </section>
   );
-  
 }
