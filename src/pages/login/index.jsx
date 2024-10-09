5467d159072b3b3687787c820b1f52f912bff87e
Leonardo Meza Pantoja <leonardomp2200@gmail.com>
2024-10-09 16:19:00 -0600

    resposive design for desktop and tablet

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 
 export default function Login() {
   return (
-    <section className=" flex flex-col items-center w-full">
-      <div className="sm:w-[360px] shadow-lg p-4 rounded-xl">
-        <div className="mt-16">
-          <h6 className="text-black/50 text-xs font-normal font-poppins leading-3 tracking-tight">
+    <section className=" flex flex-col items-center w-full  lg:flex-row-reverse lg:justify-evenly lg:items-end">
+      <div className=" shadow-lg p-4 rounded-xl md:px-16 md:w-[600px] ">
+        <div className="mt-16 ">
+          <h6 className="text-black/50 text-xs font-normal font-poppins leading-3 tracking-tight md:text-base md:mb-3 ">
             Bienvenido de vuelta!!
           </h6>
-          <h1 className="text-black text-3xl font-semibold font-poppins">
+          <h1 className="text-black text-3xl font-semibold font-poppins md:text-6xl md:mb-7">
             Iniciar sesion
           </h1>
         </div>
@@ -31,14 +31,14 @@ export default function Login() {
               placeholder="Ingresa tu correo"
               sx={{ mb: "15px" }}
             />
-            <div className="flex justify-between  items-center">
+            <div className="flex justify-between  items-center ">
               <label
                 htmlFor="Contraseña"
                 className="text-black  text-base font-poppins font-light " // verificar por que
               >
                 Contraseña
               </label>
-              <p className="text-black text-xs font-light font-poppins opacity-50">
+              <p className="text-black text-xs font-light font-poppins opacity-50 lg:text-sm">
                 ¿Ovidaste tu contraseña
               </p>
             </div>
@@ -51,12 +51,12 @@ export default function Login() {
             />
           </div>
 
-          <div className="flex justify-center mt-5 mb-5">
+          <div className="flex justify-center mt-5 mb-5 lg:mb-10">
             <Button
               endIcon={<ArrowForward />}
               sx={{
                 borderRadius: "12px",
-                py: "4px",
+                py: { sm: "4px", md: "8px" },
                 px: "28px",
                 fontWeight: "700",
                 fontSize: "14px",
@@ -67,7 +67,7 @@ export default function Login() {
             </Button>
           </div>
 
-          <div className="flex flex-col items-center ">
+          <div className="flex flex-col items-center md:flex-row md:justify-center md:gap-3 md:mb-20">
             <p className="text-black/20 text-sm font-normal font-poppins leading-3 tracking-wide">
               ¿Aun no estas registrado?
             </p>
@@ -79,7 +79,7 @@ export default function Login() {
         {/**Fin form */}
       </div>
 
-      <div className="w-72">
+      <div className="w-72 md:w-[400px]">
         <Image
           width={550}
           height={440}
