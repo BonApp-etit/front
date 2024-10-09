df1e5853645dd8f5e1ba6e4f7b292594eebb02e9
Leonardo Meza Pantoja <leonardomp2200@gmail.com>
2024-10-08 21:53:41 -0600

    conditional rendering with hook useMediaQuery from Material UI

--- src/components/landing/FirstSection.jsx
+++ src/components/landing/FirstSection.jsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import { Button } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import { useMediaQuery } from "@mui/material";
 export default function First() {
   const theme = useTheme();
+  const recomendations = useMediaQuery("(min-width:1200px)");
   return (
-    <section className=" lg:bg-[#FFF1ED] lg:py-24 lg:flex lg:justify-center md:flex md:justify-center sm:flex sm:justify-center sm:p-4">
+    <section className=" lg:bg-[#FFF1ED] lg:py-24 flex justify-center p-4">
       {/* Contenedor exterior que extiende el fondo */}
 
-      <div className=" sm:max-w-96 md:max-w-3xl  md:px-24 lg:flex lg:justify-evenly  lg:items-center lg:max-w-7xl lg:px-[20px] ">
+      <div className=" sm:max-w-96 md:max-w-3xl  md:px-24 lg:flex lg:justify-evenly  lg:items-center lg:max-w-7xl lg:px-[50px] ">
         <div className=" lg:flex lg:flex-col ">
           {/**Title and subtittle */}
-          <div className=" sm:mb-8 sm:gap-4 text-black   flex flex-col md:mt-8 md:text-center sm:text-center lg:text-left lg:gap-4  lg:mb-12 ">
-            <h1 className="  sm:text-3xl font-bold font-roboto  md:text-5xl md:font-poppins lg:text-[64px] lg:font-roboto">
+          <div className=" mb-8 gap-4 text-black   flex flex-col md:mt-8 md:text-center sm:text-center xs:text-center lg:text-left lg:gap-4  lg:mb-12 ">
+            <h1 className="  text-3xl font-bold font-roboto  md:text-5xl md:font-poppins lg:text-[64px] lg:font-roboto">
               Ordena fácilmente, disfruta rápidamente
             </h1>
             <p className=" text-base font-normal font-roboto md:text-xl lg:text-2xl  ">
@@ -46,44 +48,54 @@ export default function First() {
                   fontSize: "1rem",
                   fontWeight: "700",
                 },
+                [theme.breakpoints.down("sm")]: {
+                  py: "12px", // Cambios para tablet (menor a 768px)
+                  px: "32px",
+                  borderRadius: "45px",
+                  fontSize: "1rem",
+                  fontWeight: "700",
+                },
               }}
             >
               ¡Solicita una demo!
             </Button>
           </div>
-          <div className="flex justify-start">
-            <div className="flex -space-x-6 ">
-              <Image
-                src="/assets/pp1.svg"
-                alt="User1"
-                width={63}
-                height={63}
-                className=" rounded-full border-2 border-white"
-              />
-              <Image
-                src="/assets/pp2.svg"
-                alt="User2"
-                width={63}
-                height={63}
-                className="rounded-full border-2 border-white"
-              />
-              <Image
-                src="/assets/pp3.svg"
-                alt="User3"
-                width={63}
-                height={63}
-                className="rounded-full border-2 border-white"
-              />
+
+          {recomendations && (
+            <div className="flex justify-start">
+              <div className="flex -space-x-6 ">
+                <Image
+                  src="/assets/pp1.svg"
+                  alt="User1"
+                  width={63}
+                  height={63}
+                  className=" rounded-full border-2 border-white"
+                />
+                <Image
+                  src="/assets/pp2.svg"
+                  alt="User2"
+                  width={63}
+                  height={63}
+                  className="rounded-full border-2 border-white"
+                />
+                <Image
+                  src="/assets/pp3.svg"
+                  alt="User3"
+                  width={63}
+                  height={63}
+                  className="rounded-full border-2 border-white"
+                />
+              </div>
+              <div className=" flex flex-col ext-xl font-roboto items-center ">
+                <span className="font-bold  text-black">
+                  Nuestros usuarios felices
+                </span>
+                <span className="text-black/50  font-normal ">
+                  ⭐ 4.8 (450+ reviews)
+                </span>
+              </div>
             </div>
-            <div className=" flex flex-col ext-xl font-roboto items-center ">
-              <span className="font-bold  text-black">
-                Nuestros usuarios felices
-              </span>
-              <span className="text-black/50  font-normal ">
-                ⭐ 4.8 (450+ reviews)
-              </span>
-            </div>
-          </div>
+          )}
         </div>
 
         {/* Right Side: Chef Image */}
