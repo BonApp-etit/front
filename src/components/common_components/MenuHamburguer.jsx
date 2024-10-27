c92a9a560b5f3176f3d9c1b1d8e1c1ec9de4db28
Leonardo <leonardomp2200@gmail.com>
2024-10-26 18:19:23 -0600

    Creation NavBar

---
+++ src/components/common_components/MenuHamburguer.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+export default function MenuHamburguer() {
+  return (
+    <Image
+      width={24}
+      height={24}
+      src="/assets/NavBar/menuMobile.svg"
+      alt="Logo"
+    />
+  );
+}
---
+++ src/pages/CreationComponents/index.jsx
@@ -0,0 +1,12 @@
+import NavBar from "@/components/common_components/NavBar";
+
+export default function Components (){
+    return (
+        <section>
+            <NavBar></NavBar>
+            
+
+        </section>
+        
+    )
+}
