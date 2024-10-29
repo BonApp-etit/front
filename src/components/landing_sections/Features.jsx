f045be644d020e590d84bf59ebc2e7179543ad32
Leonardo <leonardomp2200@gmail.com>
2024-10-29 13:55:25 -0600

    Creation of Feature card. Mobile First design and swiper implemented

--- src/components/landing_sections/Features.jsx
+++ src/components/landing_sections/Features.jsx
@@ -1,67 +1,91 @@
 import React from "react";
 import FeaturesCards from "./FeaturesCards";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const features = [
   {
-    image: "/assets/thirdFirst.svg",
-    title: "Procesos de Pago Seguros y Rapidos",
+    image: "/assets/FeaturesCardsImages/QRcode.svg",
+    title: "Codigo QR ",
     description:
-      "Añade tu metodo de pago favorito y confirma tu compra con un solo click",
+      "Deja que tus clientes accedan a tu menu de forma mas rapida y sencilla",
   },
-  // {
-  //   image: "/assets/thirdSecond.svg",
-  //   title: "Procesos de Pago Seguros y Rápidos",
-  //   description:
-  //     "Añade tu método de pago favorito y confirma tu compra en un solo clic.",
-  // },
-  // {
-  //   image: "/assets/thirdThird.svg",
-  //   title: "Ordena al Instante",
-  //   description:
-  //     "Deja que tus clientes ordenen sin necesidad de esperar a un mesero.",
-  // },
-  // {
-  //   image: "/assets/thirdFourth.svg",
-  //   title: "Historial de Pedidos",
-  //   description:
-  //     "Acceso rápido a los pedidos anteriores para facilitar la repetición de favoritos.",
-  // },
-  // {
-  //   image: "/assets/thirdFive.svg",
-  //   title: "Notificaciones en Tiempo Real",
-  //   description:
-  //     "Los clientes reciben actualizaciones sobre el estado de su pedido.",
-  // },
-  // {
-  //   image: "/assets/thirdSix.svg",
-  //   title: "Menú Digital",
-  //   description:
-  //     "Un menú accesible desde la app, con descripciones claras de cada plato.",
-  // },
+  {
+    image: "/assets/FeaturesCardsImages/Credit-card.svg",
+    title: "Procesos de Pago Seguros y Rápidos",
+    description:
+      "Añade tu método de pago favorito y confirma tu compra en un solo clic.",
+  },
+  {
+    image: "/assets/FeaturesCardsImages/Order.svg",
+    title: "Ordena al Instante",
+    description:
+      "Deja que tus clientes ordenen sin necesidad de esperar a un mesero.",
+  },
+  {
+    image: "/assets/FeaturesCardsImages/Search.svg",
+    title: "Historial de Pedidos",
+    description:
+      "Acceso rápido a los pedidos anteriores para facilitar la repetición de favoritos.",
+  },
+  {
+    image: "/assets/FeaturesCardsImages/Notifications.svg",
+    title: "Notificaciones en Tiempo Real",
+    description:
+      "Los clientes reciben actualizaciones sobre el estado de su pedido.",
+  },
+  {
+    image: "/assets/FeaturesCardsImages/Drop-down.svg",
+    title: "Menú Digital",
+    description:
+      "Un menú accesible desde la app, con descripciones claras de cada plato.",
+  },
 ];
 
 export default function Features() {
   return (
     <>
-      <section className="bg-white px-4 py-12">
+      <section className="bg-white px-4">
         {/* Título Principal */}
 
         <div className="mb-8 text-center">
-          <h2 className="font-poppins text-2xl font-bold text-black">
+          <h2 className="font-poppins text-2xl font-bold text-black md:text-3xl md:leading-10 lg:text-5xl lg:leading-[63px]">
             Funciones destacadas que facilitan tu experiencia
           </h2>
         </div>
 
         {/* Sección de Tarjetas */}
         <div className="">
-          {features.map((feature, index) => (
-            <FeaturesCards
-              key={index}
-              image={feature.image}
-              title={feature.title}
-              description={feature.description}
-            />
-          ))}
+          <Swiper
+            pagination={{ clickable: true }}
+            breakpoints={{
+              360: {
+                slidesPerView: 1.4,
+                centeredSlides: true,
+                spaceBetween: 20, //40
+              },
+              744: {
+                slidesPerView: 4,
+                spaceBetween: 25,
+                centeredSlides: true,
+              },
+              1200: {
+                slidesPerView: 4,
+                spaceBetween: 390,
+                centeredSlides: false,
+              },
+            }}
+          >
+            {features.map((feature, index) => (
+              <SwiperSlide key={index}>
+                <FeaturesCards
+                  image={feature.image}
+                  title={feature.title}
+                  description={feature.description}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </section>
     </>
--- src/components/landing_sections/FeaturesCards.jsx
+++ src/components/landing_sections/FeaturesCards.jsx
@@ -3,32 +3,34 @@ import Image from "next/image";
 
 const FeatureCard = ({ image, title, description }) => {
   return (
-    <div className="relative w-[220px] rounded-[20px] bg-white px-5 shadow">
-      {/* Esquina de la tarjeta */}
-      <div className="absolute right-0 top-0 h-[38px] w-[43px] rounded-bl-[21px] rounded-tr-[19px] bg-cs500"></div>
+    <div className="flex justify-center">
+      <div className="relative mb-4 h-[280px] w-[220px] rounded-[20px] bg-white px-3 shadow-lg">
+        {/* Esquina de la tarjeta */}
+        <div className="absolute right-0 top-0 h-[38px] w-[43px] rounded-bl-[21px] rounded-tr-[19px] bg-cs500"></div>
 
-      {/* Imagen */}
-      <div className="mx-auto w-[150px]">
-        <Image
-          src={image}
-          alt={title}
-          width={223}
-          height={262}
-          layout="responsive"
-        />
-      </div>
+        {/* Imagen */}
+        <div className="mx-auto mb-2 mt-5 w-[150px]">
+          <Image
+            src={image}
+            alt={title}
+            width={223}
+            height={262}
+            layout="responsive"
+          />
+        </div>
 
-      {/* Contenedor para centrar título y descripción */}
-      <div className="flex flex-col items-center">
-        {/* Título */}
-        <h3 className="text-center font-poppins text-base font-semibold text-black">
-          {title}
-        </h3>
+        {/* Contenedor para centrar título y descripción */}
+        <div className="flex flex-col items-center gap-2">
+          {/* Título */}
+          <h3 className="text-center font-poppins text-base font-semibold text-black">
+            {title}
+          </h3>
 
-        {/* Descripción */}
-        <p className="text-center font-roboto text-xs font-semibold text-[#555555]">
-          {description}
-        </p>
+          {/* Descripción */}
+          <p className="mb-3 text-center font-roboto text-[11px] font-semibold leading-tight text-[#555555]">
+            {description}
+          </p>
+        </div>
       </div>
     </div>
   );
