d880526b4aecb240d7133c46c7e5b3e0c1b71954
Leonardo <leonardomp2200@gmail.com>
2024-10-28 19:20:48 -0600

    Swiper implementations on cards at about section

--- src/components/landing_sections/About.jsx
+++ src/components/landing_sections/About.jsx
@@ -1,37 +1,27 @@
 import React from "react";
 import TarjetasPrimeras from "./FirstCard";
 
-const tarjetasData = [
-  { texto: "Recetas de calidad", src: "/assets/secondFirst.svg" },
-  { texto: "Entrega rápida", src: "/assets/secondSecond.svg" },
-  { texto: "Sabor garantizado", src: "/assets/secondThird.svg" },
-];
-
 const AboutSection = () => {
   return (
-    <section className="bg-white px-4 py-12 font-sans">
+    <section className="bg-white px-4 py-12">
       {/* Título Principal */}
       <div className="mb-8 text-center">
-        <p className="mb-4 font-poppins text-[#FF441F]">QUE HACEMOS</p>
-        <h2 className="font-poppins text-[48px] font-bold text-black">
-          Facilitamos el proceso de ordenar comida en <br /> restaurantes.
+        <p className="mb-2 font-poppins text-base font-semibold text-[#d52100]">
+          QUE HACEMOS
+        </p>
+        <h2 className="font-poppins text-2xl font-bold text-black md:text-[32px] lg:text-5xl">
+          Facilitamos el proceso de ordenar comida en restaurantes.
         </h2>
-        <p className="mt-4 font-poppins text-[16px] text-gray-500">
+
+        <p className="mt-4 font-roboto text-base font-normal text-[#3f3d56]">
           Hacemos que tus usuarios seleccionen sus platos favoritos y realicen
           sus pedidos de manera rápida y sencilla.
         </p>
       </div>
 
       {/* Sección de Iconos */}
-      <div className="mx-[120px] grid grid-cols-1 gap-8 md:grid-cols-3">
-        {/* Recetas de Calidad */}
-        {tarjetasData.map((tarjeta, index) => (
-          <TarjetasPrimeras
-            key={index}
-            texto={tarjeta.texto}
-            src={tarjeta.src}
-          />
-        ))}
+      <div>
+        <TarjetasPrimeras />
       </div>
     </section>
   );
