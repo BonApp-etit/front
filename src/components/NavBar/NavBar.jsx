9ebd5fa16f5a3a878c1ea5183b254567ed7944d8
Leonardo <leonardomp2200@gmail.com>
2024-10-29 21:43:42 -0600

    Navbar width for breakpoint done 1200px desktop, 632px tablet 328px mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,12 +4,14 @@ import Links from "./Links";
 
 const MenuMaterial = () => {
   return (
-    <nav className="mb-1 flex items-center justify-between bg-white p-4 shadow-md md:px-14 lg:px-[120px]">
-      <Logo />
-      <Links />
-      <MenuHamburguer
-        items={["Iniciar sesion", "Registrate", "Beneficios", "Servicios"]}
-      />
+    <nav className="flex justify-center bg-white shadow-md">
+      <section className="mb-1 flex w-[328px] items-center justify-between py-4 md:w-[632px] lg:w-[1200px]">
+        <Logo />
+        <Links />
+        <MenuHamburguer
+          items={["Iniciar sesion", "Registrate", "Beneficios", "Servicios"]}
+        />
+      </section>
     </nav>
   );
 };
