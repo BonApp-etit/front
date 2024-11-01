a1f3d861cd403fcfbb1b4eea5803d0d4d3cd12b5
Leonardo <leonardomp2200@gmail.com>
2024-11-01 10:49:05 -0600

    Changes of responsive design. Implementation of min-width and max-width

--- src/components/common_components/Form.jsx
+++ src/components/common_components/Form.jsx
@@ -11,8 +11,8 @@ export default function Form({
   return (
     <section>
       <div className="mx-auto lg:mt-20 lg:w-[1200px]">
-        <div className="flex w-full flex-col items-center justify-center lg:flex-row-reverse lg:justify-between">
-          <div className="w-[328px] rounded-[20px] px-2 py-5 shadow-lg md:w-[632px] md:px-[70px] md:py-[50px] lg:w-[660px]">
+        <div className="flex w-full flex-col items-center justify-center px-4 md:px-14 lg:flex-row-reverse lg:gap-20 lg:px-0">
+          <div className="my-4 w-full max-w-[400px] rounded-[20px] px-2 py-5 shadow-lg md:min-w-[500px] md:max-w-[700px] md:px-[70px] md:py-[50px]">
             <div className="mb-2 flex flex-col gap-1">
               <h6 className="font-poppins text-xs font-normal leading-3 tracking-tight text-black/50 md:mb-3 md:text-base">
                 {subtitleTop}
@@ -31,7 +31,8 @@ export default function Form({
             {/**Fin form */}
           </div>
           {/* Punto de control. Img 2div  */}
-          <div className="mx-auto w-72 md:w-[400px]">
+
+          <div className="mx-auto w-full min-w-[100px] max-w-[320px] md:min-w-[300px] md:max-w-[400px] lg:max-w-[500px]">
             <Image
               width={550}
               height={440}
