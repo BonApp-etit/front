9cabb02c5d00eb8802ce59c166e22c8127643948
Leonardo <leonardomp2200@gmail.com>
2024-11-01 14:33:44 -0600

    Adition of validation Schemas for login form with Formik and responsive design

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,14 @@
-import MenuHamburguer from "./MenuMobile";
+import MenuMobile from "./MenuMobile";
 import Logo from "./Logo";
 import Links from "./Links";
 
 const MenuMaterial = () => {
   return (
-    <nav className="flex justify-center bg-white p-4 shadow-md">
+    <nav className="flex justify-center bg-white p-4 shadow-md md:px-14">
       <section className="mb-1 flex w-full min-w-[328px] max-w-[400px] items-center justify-between md:min-w-[632px] md:max-w-[700px] lg:max-w-[1200px]">
         <Logo />
         <Links />
-        <MenuHamburguer
+        <MenuMobile
           items={["Iniciar sesion", "Registrate", "Beneficios", "Servicios"]}
         />
       </section>
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,8 @@
 import Form from "../../components/common_components/Form";
 import InputContained from "@/components/common_components/InputContained";
 import ButtonContained from "@/components/common_components/ButtonContained";
-
+import { Formik, Form as FormikForm, Field, ErrorMessage } from "formik";
+import { loginSchema } from "@/hooks/validationSchemas";
 import NavBar from "@/components/NavBar/NavBar";
 
 export default function SignUp() {
@@ -9,50 +10,81 @@ export default function SignUp() {
     <main>
       <NavBar />
 
-      <Form
-        tittle="Iniciar sesion"
-        subtitleTop="Bienvenido de vuelta!!"
-        subtitleBottom=""
-        src="/assets/Login/chef.svg"
-        alt="SignUp"
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={loginSchema}
+        onSubmit={(values) => {
+          console.log("Datos del formulario", values);
+        }}
       >
-        <InputContained
-          label="Correo electronico"
-          placeholder="Ingresa tu correo electronico"
-          type="text"
-        />
-
-        {/* Contenedor para el Input y el componente adicional */}
-        <div className="mb-4">
-          <div className="flex items-center justify-between">
-            <label className="font-poppins text-sm font-light text-black md:text-base lg:text-lg">
-              Contraseña
-            </label>
-            <a
-              href="#"
-              className="font-poppins text-xs text-black/50 hover:underline md:text-base lg:text-base"
-            >
-              Olvidaste tu contraseña?
-            </a>
-          </div>
-          <InputContained placeholder="Ingresa tu contraseña" type="password" />
-        </div>
-        <div className="mb-5 mt-5 flex justify-center lg:mb-10">
-          <ButtonContained text="Entrar"></ButtonContained>
-        </div>
-        <div className="flex justify-center gap-2">
-          <p className="font-poppins text-xs font-normal leading-snug tracking-tight text-black/50 md:text-base">
-            ¿Aun no estas registrado?
-          </p>{" "}
-          <a
-            className="font-poppins text-xs font-normal leading-snug tracking-tight text-cs500 hover:underline md:text-base"
-            href="#"
+        {({ handleSubmit }) => (
+          <Form
+            tittle="Iniciar sesión"
+            subtitleTop="Bienvenido de vuelta!!"
+            subtitleBottom=""
+            src="/assets/Login/chef.svg"
+            alt="SignUp"
           >
-            {" "}
-            Crear cuenta
-          </a>
-        </div>
-      </Form>
+            <FormikForm onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <Field
+                  name="email"
+                  as={InputContained}
+                  label="Correo electrónico"
+                  placeholder="Ingresa tu correo electrónico"
+                  type="text"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="p"
+                  className="font-roboto text-sm text-red-500 md:text-base lg:text-lg"
+                />
+              </div>
+
+              <div className="mb-4">
+                <div className="flex items-center justify-between">
+                  <label className="font-poppins text-sm font-light text-black md:text-base lg:text-lg">
+                    Contraseña
+                  </label>
+                  <a
+                    href="#"
+                    className="font-poppins text-xs text-black/50 hover:underline md:text-base lg:text-base"
+                  >
+                    Olvidaste tu contraseña?
+                  </a>
+                </div>
+                <Field
+                  name="password"
+                  as={InputContained}
+                  placeholder="Ingresa tu contraseña"
+                  type="password"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="font-roboto text-sm text-red-500 md:text-base lg:text-lg"
+                />
+              </div>
+
+              <div className="mb-5 mt-5 flex justify-center lg:mb-10">
+                <ButtonContained text="Entrar" type="submit" />
+              </div>
+            </FormikForm>
+
+            <div className="flex justify-center gap-2">
+              <p className="font-poppins text-xs font-normal leading-snug tracking-tight text-black/50 md:text-base">
+                ¿Aun no estás registrado?
+              </p>
+              <a
+                className="font-poppins text-xs font-normal leading-snug tracking-tight text-cs500 hover:underline md:text-base"
+                href="#"
+              >
+                Crear cuenta
+              </a>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </main>
   );
 }
