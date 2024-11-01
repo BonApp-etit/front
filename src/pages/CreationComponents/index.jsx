01fe4893be5b1723066072382088c8ddb414e8e0
Leonardo <leonardomp2200@gmail.com>
2024-10-31 19:49:50 -0600

    Creation of signUp Form done

--- src/pages/CreationComponents/index.jsx
+++ src/pages/CreationComponents/index.jsx
@@ -1,9 +1,44 @@
+import Form from "./Form";
+import InputTailwind from "@/components/common_components/InputTailwind";
+import ButtonTailwind from "@/components/common_components/ButtonTailwind";
+import SignInOPtions from "@/components/common_components/SignInOptions";
+
 import NavBar from "@/components/NavBar/NavBar";
 
-export default function Components() {
+export default function SignUp() {
   return (
-    <section>
+    <main>
       <NavBar />
-    </section>
+
+      <Form
+        tittle="Registrate"
+        subtitleTop=""
+        subtitleBottom="Registrate y dile que si a miles de clientes satisfechos"
+        src="/assets/SignUp/SignUp.svg"
+        alt="SignUp"
+      >
+        <InputTailwind
+          label="Nombre completo"
+          placeholder="Ingresa tu nombre completo"
+          type="text"
+        />
+        <InputTailwind
+          label="Correo electronico"
+          placeholder="Ingresa tu correo electronico"
+          type="text"
+        />
+
+        <InputTailwind
+          label="Contraseña"
+          placeholder="Ingresa tu contraseña"
+          type="text"
+        />
+        <div className="mb-5 mt-5 flex justify-center lg:mb-10">
+          <ButtonTailwind text="Registrarse"></ButtonTailwind>
+        </div>
+
+        <SignInOPtions />
+      </Form>
+    </main>
   );
 }
