ad844cb6f18ac2dc25e7d4d6d2b0a95ee9ee13c6
Leonardo <leonardomp2200@gmail.com>
2024-11-04 23:52:35 -0600

    Creation of button group categories

---
+++ src/components/administration_menu/CategoriesButton.jsx
@@ -0,0 +1,16 @@
+export default function CategoriesButton({ names }) {
+  return (
+    <div className="flex">
+      {names.map((name, index) => (
+        <button
+          key={index}
+          className="inline-flex h-[26px] items-center justify-center rounded border border-cs500 bg-white px-2 py-1 text-cstext shadow-md transition-all duration-150 ease-in-out hover:text-cs500 hover:shadow-2xl active:text-cs500 active:shadow-none lg:px-3 lg:py-4"
+        >
+          <span className="font-poppins text-xs font-semibold lg:text-base">
+            {name}
+          </span>
+        </button>
+      ))}
+    </div>
+  );
+}
