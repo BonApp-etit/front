220a80cc9d984fc6bb231db08cbed2e871feb1cd
Leonardo <leonardomp2200@gmail.com>
2024-11-05 14:39:58 -0600

    SearchBar responsive done

--- src/components/administration_menu/SearchBar.jsx
+++ src/components/administration_menu/SearchBar.jsx
@@ -2,8 +2,11 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 export default function SearchBar() {
   return (
     <section>
-      <div className="flex items-center gap-2 rounded-lg border-2 border-cs500 px-3 py-1">
-        <MagnifyingGlassIcon className="h-5 w-5 text-cs500" strokeWidth={2} />
+      <div className="flex w-full items-center gap-2 rounded-lg border-2 border-cs500 px-3 py-1">
+        <MagnifyingGlassIcon
+          className="h-4 w-4 text-cs500 lg:h-5 lg:w-5"
+          strokeWidth={2}
+        />
         <input
           className="flex-1 font-poppins text-xs focus:outline-none"
           placeholder="Buscar platillo"
