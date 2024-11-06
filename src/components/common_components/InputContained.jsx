7cd8a834da762124eaca74a73f484be9a07c2209
Leonardo <leonardomp2200@gmail.com>
2024-11-06 13:16:31 -0600

    Creation of variant intput outlined

--- src/components/common_components/InputContained.jsx
+++ src/components/common_components/InputContained.jsx
@@ -1,4 +1,15 @@
-export default function InputContained({ label, placeholder, type, ...props }) {
+export default function InputContained({
+  label,
+  placeholder,
+  type,
+  variant,
+  ...props
+}) {
+  const variantClass =
+    variant === "outlined"
+      ? "mt-1 w-full rounded-lg border border-cs400 bg-[#FFF6F4] px-4 py-2 font-poppins text-xs font-normal text-black  placeholder:text-black/50 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-cs500 md:text-sm lg:text-base"
+      : "mt-1 w-full rounded-lg border border-[#FFF6F4] bg-[#FFF6F4] px-4 py-2 font-poppins text-xs font-normal text-black placeholder:text-black/50 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-cs500 md:text-sm lg:text-base";
+
   return (
     <div className="mb-1">
       <label
@@ -11,9 +22,9 @@ export default function InputContained({ label, placeholder, type, ...props }) {
       <input
         id={label}
         type={type}
-        placeholder={placeholder}
         {...props}
-        className="mt-1 w-full rounded-lg border border-[#FFF6F4] bg-[#FFF6F4] px-4 py-2 font-poppins text-xs font-normal text-black placeholder:text-black/50 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-cs500 md:text-sm lg:text-base"
+        placeholder={placeholder}
+        className={variantClass}
       />
     </div>
   );
