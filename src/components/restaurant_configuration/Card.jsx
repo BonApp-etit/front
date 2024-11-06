5156ea7c3c30b9b24dbf77a67a3951d517bbffcc
Leonardo <leonardomp2200@gmail.com>
2024-11-06 16:41:29 -0600

    restaurant configuration page done

--- src/components/restaurant_configuration/Card.jsx
+++ src/components/restaurant_configuration/Card.jsx
@@ -1,55 +1,45 @@
 import { useState } from "react";
 import { ArrowRightIcon } from "@heroicons/react/20/solid";
-import InputContained from "../common_components/InputContained";
 import ButtonContained from "../common_components/ButtonContained";
+import LayoutCard from "../common_components/LayoutCard";
 
-export default function Card() {
+export default function Card({ titleInfo, info, children }) {
   const [isExpanded, setIsExpanded] = useState(false);
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
   };
   return (
-    <div className="w-full">
-      <section className="rounded-[20px] bg-[#fcfcfc] px-[30px] py-4 shadow">
-        <div className="flex items-center justify-between">
-          <div className="flex flex-col">
-            <h6 className="font-poppins text-base font-semibold leading-tight text-[#222222]">
-              Nombre del restaurante
-            </h6>
-            <p className="font-poppins text-sm font-medium leading-[18px] text-[#717171]">
-              Mr.Burguer
-            </p>
-          </div>
-          <button onClick={toggleExpand}>
-            <ArrowRightIcon
-              strokeWidth={2}
-              className={`h-7 w-7 text-cs500 transition-transform duration-300 ease-in-out ${isExpanded ? "rotate-90" : ""}`}
-            />
-          </button>
+    <LayoutCard>
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col">
+          <h6 className="font-poppins text-base font-semibold leading-tight text-[#222222] lg:text-lg">
+            {titleInfo}
+          </h6>
+          <p className="font-poppins text-sm font-medium leading-[18px] text-[#717171] lg:text-base">
+            {info}
+          </p>
         </div>
+        <button onClick={toggleExpand}>
+          <ArrowRightIcon
+            strokeWidth={2}
+            className={`h-7 w-7 text-cs500 transition-transform duration-300 ease-in-out md:h-8 md:w-8 lg:h-9 lg:w-9 ${isExpanded ? "rotate-90" : ""}`}
+          />
+        </button>
+      </div>
 
-        <section
-          className={`transform overflow-hidden transition-all duration-300 ease-in-out ${
-            isExpanded ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
-          }`}
-        >
-          <div className="mt-4">
-            <InputContained
-              label="Nombre del restaurante"
-              placeholder="Mr.Burguer"
-              type="text"
-              variant="outlined"
-            />
-            <div className="my-3 flex w-full justify-between">
-              <ButtonContained
-                variant="generalPoppins"
-                text="Guardar cambios"
-              />
-              <ButtonContained variant="text" text="Descartar" />
-            </div>
+      <section
+        className={`transform overflow-hidden transition-all duration-300 ease-in-out ${
+          isExpanded ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
+        }`}
+      >
+        <div className="mt-4">
+          {children}
+          <div className="my-3 flex w-full justify-between md:justify-end md:gap-7">
+            <ButtonContained variant="generalPoppins" text="Guardar cambios" />
+            <ButtonContained variant="text" text="Descartar" />
           </div>
-        </section>
+        </div>
       </section>
-    </div>
+    </LayoutCard>
   );
 }
--- src/pages/restaurant_configuration/index.jsx
+++ src/pages/restaurant_configuration/index.jsx
@@ -1,37 +1,54 @@
 import Card from "@/components/restaurant_configuration/Card";
 import Image from "next/image";
 import NavBar from "@/components/NavBar/NavBar";
+import MainLayout from "@/components/common_components/MainLayout";
+import InputContained from "@/components/common_components/InputContained";
+import UploadImageCard from "@/components/common_components/UploadImageCard";
+
+const DB = [
+  { titleInfo: "Nombre del restaurante", info: "Mr.burguer" },
+  {
+    titleInfo: "Direccion",
+    info: "Av. Adolfo López Mateos Sur 2375, Plaza del Sol, 45050 Zapopan, Jal.",
+  },
+  { titleInfo: "Telefono", info: "22-54-56-78" },
+  { titleInfo: "Correo electronico", info: "JohnDoue123@gmail.com" },
+];
 export default function RestauranConfiguration() {
   return (
     <main>
       <NavBar></NavBar>
-      <div className="flex justify-center bg-white p-4 shadow-md md:px-14">
-        <section className="mb-1 flex w-full max-w-[400px] flex-col md:min-w-[632px] md:max-w-[700px] lg:max-w-[1200px]">
-          <section className="mb-5 flex items-center justify-start gap-4">
-            <div className="w-[100px]">
-              <Image
-                width={40}
-                height={40}
-                src="/assets/AdministrationMenu/RestaurantLogo.svg"
-                alt="Logo restaurant"
-                layout="responsive"
-              ></Image>
-            </div>
-            <h1 className="font-poppins text-2xl font-medium text-black">
-              Mi restaurante
-            </h1>
-          </section>
-
-          <section className="flex w-full flex-col gap-2">
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-          </section>
+      <MainLayout>
+        <section className="mb-5 flex items-center justify-start gap-4 lg:gap-8">
+          <div className="w-[100px] md:w-[150px] lg:w-[150px]">
+            <Image
+              width={40}
+              height={40}
+              src="/assets/AdministrationMenu/RestaurantLogo.svg"
+              alt="Logo restaurant"
+              layout="responsive"
+            ></Image>
+          </div>
+          <h1 className="font-poppins text-2xl font-medium text-black md:text-4xl lg:text-5xl">
+            Mi restaurante
+          </h1>
         </section>
-      </div>
+        <section className="flex w-full flex-col items-center gap-2">
+          {DB.map((module, idx) => (
+            <Card key={idx} titleInfo={module.titleInfo} info={module.info}>
+              <InputContained
+                label={module.titleInfo}
+                placeholder={module.info}
+                type="text"
+                variant="outlined"
+              />
+            </Card>
+          ))}
+          <Card titleInfo="Logo del restaurant" info="Image">
+            <UploadImageCard></UploadImageCard>
+          </Card>
+        </section>
+      </MainLayout>
     </main>
   );
 }
