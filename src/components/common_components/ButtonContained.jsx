4fd54ca30cbbf8d7ec5a86cd3ed6c8cbb6b5fccf
Leonardo <leonardomp2200@gmail.com>
2024-11-06 13:15:49 -0600

    Creation of variant text button

--- src/components/common_components/ButtonContained.jsx
+++ src/components/common_components/ButtonContained.jsx
@@ -18,7 +18,15 @@ export default function ButtonContained({
           {showIcon && <ArrowRightIcon className="ml-2 h-5 w-5" />}
         </button>
       )}
-
+      {variant === "text" && (
+        <button
+          type={type}
+          className={`flex items-center justify-center rounded-lg bg-white px-3 py-2 font-poppins text-sm font-semibold text-cs500 shadow-md transition-all duration-150 ease-in-out hover:bg-cs700 hover:shadow-2xl active:bg-cs900 active:shadow-none md:text-base lg:text-lg ${tailwindClasses}`}
+        >
+          <span>{text}</span>
+          {showIcon && <ArrowRightIcon className="ml-2 h-5 w-5" />}
+        </button>
+      )}
       {variant === "generalUbuntu" && (
         <button
           type={type}
