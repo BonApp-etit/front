0b4dc0f1896cde9e932b76689c3e2b6d57b6072e
Leonardo <leonardomp2200@gmail.com>
2024-11-07 18:03:38 -0600

    Implementation of stepper component

--- src/pages/choose_products/index.jsx
+++ src/pages/choose_products/index.jsx
@@ -2,15 +2,31 @@ import NavBar from "@/components/NavBar/NavBar";
 import MainLayout from "@/components/common_components/MainLayout";
 import LayoutCard from "@/components/common_components/LayoutCard";
 import CardContent from "@/components/UserOrder/CardContent";
+import { SwiperSlide } from "swiper/react";
 import ButtonContained from "@/components/common_components/ButtonContained";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Scrollbar } from "swiper/modules";
+import React, { useState } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
-import Arrow from "@/components/administration_menu/Arrow";
 import SwiperConfiguration from "@/components/administration_menu/SwiperConfiguration";
+import Stepper from "@/components/common_components/Stepper";
 
 export default function ChooseProducts(title, subtitle) {
+  const [currentStep, setCurrentStep] = useState(1); // Paso inicial
+
+  const stepsLength = 5; // Número total de pasos
+
+  const handleNext = () => {
+    if (currentStep < stepsLength) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentStep > 1) {
+      setCurrentStep(currentStep - 1);
+    }
+  };
+
   return (
     <main>
       <NavBar></NavBar>
@@ -47,6 +63,42 @@ export default function ChooseProducts(title, subtitle) {
               </LayoutCard>
             </SwiperSlide>
           </SwiperConfiguration>
+        </div>
+        <div className="mt-2">
+          <Stepper currentStep={currentStep} />
+        </div>
+        <div>
+          <LayoutCard>
+            <div className="mx-auto max-w-[320px] md:max-w-[600px]">
+              <div className="flex justify-evenly">
+                <p className="font-ubuntu text-base font-normal leading-relaxed tracking-tight text-black">
+                  Total
+                </p>
+                <div className="font-ubuntu text-base leading-relaxed tracking-tight text-cs600">
+                  <span className="font-normal">$</span>
+                  <span className="mr-1 font-bold">359</span>
+                  <span className="font-normal">MXN</span>
+                </div>
+              </div>
+
+              <div className="flex justify-evenly">
+                <ButtonContained
+                  onClick={handlePrev}
+                  disabled={currentStep === 1}
+                  text="Volver"
+                  variant="text"
+                  showIcon="true"
+                />
+                <ButtonContained
+                  onClick={handleNext}
+                  disabled={currentStep === stepsLength}
+                  text="Siguiente"
+                  variant="generalPoppins"
+                  showIcon="true"
+                />
+              </div>
+            </div>
+          </LayoutCard>
         </div>
       </MainLayout>
     </main>
