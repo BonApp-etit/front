03281ca93c1d19d043a28b72eff76fe4bd1399c1
Leonardo <leonardomp2200@gmail.com>
2024-11-10 01:54:39 -0600

    Incomplete order and implementation of not available card

--- src/components/UserOrder/CardContent.jsx
+++ src/components/UserOrder/CardContent.jsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import ButtonIncrease from "../administration_menu/ButtonIncrease";
+import clsx from "clsx";
 
-export default function CardContent(
+export default function CardContent({
   alt,
   src,
   price,
   deletedIngredients,
   extraIngredients,
-) {
+  isAvailable = true,
+}) {
   return (
     <section className="flex gap-2">
       <div className="h-auto w-[100px] flex-shrink-0 md:w-[220px]">
@@ -17,14 +19,26 @@ export default function CardContent(
           layout="responsive"
           alt="Hamburguer Image"
           src="/assets/userOrder/Hamburguer.svg"
-          className="object-cover"
+          className={clsx("object-cover", {
+            "saturate-0 filter": !isAvailable,
+          })}
         />
       </div>
       <div className="flex flex-col gap-2 md:gap-0">
-        <h6 className="font-ubuntu text-base font-bold text-cs600 md:text-xl">
+        <h6
+          className={clsx(
+            "font-ubuntu text-base font-bold text-cs600 md:text-xl",
+            { "text-[#B0B0B0]": !isAvailable },
+          )}
+        >
           Hamburguesa Sencilla
         </h6>
-        <div className="font-ubuntu text-sm leading-snug tracking-tight text-[#272727] md:text-lg">
+        <div
+          className={clsx(
+            "font-ubuntu text-sm leading-snug tracking-tight text-[#272727] md:text-lg",
+            { "text-[#A8A8A8]": !isAvailable },
+          )}
+        >
           <span className="font-normal">$</span>
           <span className="font-bold">145.00 </span>
           <span className="font-normal">MXN</span>
@@ -37,7 +51,7 @@ export default function CardContent(
           <p>Ingredientes extra: Queso, Tocino, Aguacate.</p>
         </div>
         <div className="flex justify-end">
-          <ButtonIncrease></ButtonIncrease>
+          <ButtonIncrease isAvailable={isAvailable}></ButtonIncrease>
         </div>
       </div>
     </section>
