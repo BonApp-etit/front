c0a5611f9cb8a42c069d7c64e08d9951ab768501
Leonardo <leonardomp2200@gmail.com>
2024-11-26 07:41:04 -0600

    Creation of Checkbox with label component

---
+++ src/components/common_components/Checkbox.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function Checkbox({ labelText, onChange }) {
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    const newChecked = !checked;
+    setChecked(newChecked);
+    if (onChange) {
+      onChange(newChecked);
+    }
+  };
+
+  return (
+    <div className="flex items-center">
+      {/* El checkbox */}
+      <input
+        type="checkbox"
+        id="checkbox"
+        checked={checked}
+        onChange={handleChange}
+        className="h-4 w-4 rounded-md border-2 border-gray-300 checked:bg-orange-500 focus:ring-0"
+      />
+      {/* El label */}
+      <label
+        htmlFor="checkbox"
+        className="ml-2 font-poppins text-sm text-cstext"
+      >
+        {labelText}
+      </label>
+    </div>
+  );
+}
+
+export default Checkbox;
