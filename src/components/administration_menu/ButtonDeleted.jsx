6435729cb76863dbf41f03a6aba84b19bcb9d0f3
Leonardo <leonardomp2200@gmail.com>
2024-11-26 12:04:48 -0600

    Effects added hover, active, and props

--- src/components/administration_menu/ButtonDeleted.jsx
+++ src/components/administration_menu/ButtonDeleted.jsx
@@ -1,7 +1,11 @@
 import { XMarkIcon } from "@heroicons/react/24/outline";
-export default function ButtonDeleted() {
+export default function ButtonDeleted({ props }) {
   return (
-    <button className="w-6 rounded-full bg-[#e01e1e] p-1 lg:w-7">
+    <button
+      type="button"
+      {...props}
+      className="w-6 rounded-full bg-[#e01e1e] p-1 hover:bg-cs600 active:bg-cs700 lg:w-7"
+    >
       <span>
         <XMarkIcon
           strokeWidth={2}
