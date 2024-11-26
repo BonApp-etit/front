7cd3f7edff90096a5fba9dc814934837d9530750
Leonardo <leonardomp2200@gmail.com>
2024-11-26 07:47:47 -0600

    Implementation of ingredients table, disgStatus component, DescriptinBox, checkBox

--- src/components/common_components/Modal.jsx
+++ src/components/common_components/Modal.jsx
@@ -1,4 +1,13 @@
 import React, { useState } from "react";
+import UploadImageCard from "./UploadImageCard";
+import InputContained from "./InputContained";
+import DescriptionBox from "./DescriptionBox";
+import Card from "../restaurant_configuration/Card";
+import Checkbox from "./Checkbox";
+import IngredientsTable from "./IngredientsTable";
+import DishStatus from "./DishStatus";
+import LayoutCard from "./LayoutCard";
+import ButtonContained from "./ButtonContained";
 
 const Modal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
@@ -9,16 +18,16 @@ const Modal = ({ isOpen, onClose }) => {
       role="dialog"
       aria-modal="true"
     >
-      <div className="relative max-h-[80vh] w-full max-w-[744px] overflow-y-auto rounded-lg bg-white p-4 shadow dark:bg-gray-700">
+      <div className="relative max-h-[80vh] w-full max-w-[744px] overflow-y-auto rounded-lg bg-white p-4 shadow">
         {/* Modal header */}
-        <div className="flex items-center justify-between rounded-t border-b p-4 dark:border-gray-600">
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-            Create New Product
+        <div className="flex items-center justify-between rounded-t border-b p-4">
+          <h3 className="font-ubuntu text-3xl font-semibold text-cstext">
+            Platillos
           </h3>
           <button
             type="button"
             onClick={onClose}
-            className="inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
+            className="inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-cs500 hover:text-white"
           >
             <svg
               className="h-3 w-3"
@@ -40,76 +49,64 @@ const Modal = ({ isOpen, onClose }) => {
 
         {/* Modal body */}
         <form className="p-4 md:p-5">
-          <div className="mb-4 grid grid-cols-2 gap-4">
-            <div className="col-span-2">
-              <label
-                htmlFor="name"
-                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Name
-              </label>
-              <input
+          <section className="lg:flex lg:gap-4">
+            <UploadImageCard text="Añade una imagen a tu platillo!"></UploadImageCard>
+            <div>
+              <InputContained
+                label="Nombre del platillo"
                 type="text"
-                id="name"
-                className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
-                placeholder="Type product name"
-                required
+                variant="outlined"
+                placeholder="Hamburguesa"
+              />
+              <InputContained
+                label="Precio"
+                type="text"
+                variant="outlined"
+                placeholder="$85"
+              />
+              <InputContained
+                label="Tiempo estimado de preparación"
+                type="text"
+                variant="outlined"
+                placeholder="30min"
+              />
+              <InputContained
+                label="kCal"
+                type="text"
+                variant="outlined"
+                placeholder="180kCal"
               />
             </div>
-            <div className="col-span-2 sm:col-span-1">
-              <label
-                htmlFor="price"
-                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Price
-              </label>
-              <input
-                type="number"
-                id="price"
-                className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-500 dark:bg-gray-600"
-                placeholder="$2999"
-                required
-              />
+          </section>
+
+          <DishStatus></DishStatus>
+
+          <DescriptionBox></DescriptionBox>
+          <section className="flex flex-col gap-3">
+            <Card
+              tailwindClasses={"!px-0"}
+              titleInfo={<Checkbox labelText="Ingredientes base" />}
+            >
+              <IngredientsTable></IngredientsTable>
+            </Card>
+
+            <Card
+              tailwindClasses={"!px-0"}
+              titleInfo={<Checkbox labelText="Ingredientes extra" />}
+            >
+              <IngredientsTable></IngredientsTable>
+            </Card>
+
+            <div className="mt-4 px-1">
+              <div className="my-3 flex w-full justify-between md:justify-end md:gap-7">
+                <ButtonContained
+                  variant="generalPoppins"
+                  text="Guardar cambios"
+                />
+                <ButtonContained variant="text" text="Descartar" />
+              </div>
             </div>
-            <div className="col-span-2 sm:col-span-1">
-              <label
-                htmlFor="category"
-                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Category
-              </label>
-              <select
-                id="category"
-                className="focus:ring-primary-500 focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-500 dark:bg-gray-600"
-              >
-                <option defaultValue="">Select category</option>
-                <option value="TV">TV/Monitors</option>
-                <option value="PC">PC</option>
-                <option value="GA">Gaming/Console</option>
-                <option value="PH">Phones</option>
-              </select>
-            </div>
-            <div className="col-span-2">
-              <label
-                htmlFor="description"
-                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Product Description
-              </label>
-              <textarea
-                id="description"
-                rows="4"
-                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600"
-                placeholder="Write product description here"
-              ></textarea>
-            </div>
-          </div>
-          <button
-            type="submit"
-            className="rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
-          >
-            Add new product
-          </button>
+          </section>
         </form>
       </div>
     </div>
