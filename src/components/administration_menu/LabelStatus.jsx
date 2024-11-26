fdf55b672d2123570c14b705444f22907b3043fc
Leonardo <leonardomp2200@gmail.com>
2024-11-26 07:36:14 -0600

    Change label to button, to indicate isAvailable

--- src/components/administration_menu/LabelStatus.jsx
+++ src/components/administration_menu/LabelStatus.jsx
@@ -1,9 +1,17 @@
-export default function LabelStatus() {
+import clsx from "clsx";
+
+export default function LabelStatus({ isAvailable, props }) {
   return (
-    <section className="inline-flex rounded-full bg-[#e60000] px-4 py-1">
+    <button
+      {...props}
+      className={clsx("inline-flex rounded-full px-4 py-1 hover:bg-cs700", {
+        "bg-[#e60000] active:bg-cs800": !isAvailable, // Agotado: rojo
+        "bg-green-700 hover:bg-green-800 active:bg-green-900": isAvailable, // Disponible: verde
+      })}
+    >
       <span className="font-ubuntu text-xs font-bold text-white lg:text-sm">
-        Agotado
+        {isAvailable ? "Disponible" : "Agotado"}
       </span>
-    </section>
+    </button>
   );
 }
