87f1c0e60aece91e83f45aef96fbe7b3f0015a78
Leonardo <leonardomp2200@gmail.com>
2024-11-27 14:23:27 -0600

    implement git Ignore for api develop

--- src/pages/kitchen_orders/index.jsx
+++ src/pages/kitchen_orders/index.jsx
@@ -1,62 +1,45 @@
 import NavBar from "@/components/NavBar/NavBar";
 import MainLayout from "@/components/common_components/MainLayout";
-
 import Order from "@/components/kitchen_order/Order";
+import StatusOrderCard from "@/components/kitchen_order/StatusOrderCard";
+import { useEffect, useState } from "react";
 
-import KitchenOrderCard from "@/components/kitchen_order/KitchenOrderCard";
-export default function kitchenOrders() {
+export default function KitchenOrders() {
+  const [ordersData, setOrdersData] = useState([]);
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await fetch("/api/kitchenOrders");
+        if (!response.ok) throw new Error("Error al obtener los datos");
+        const data = await response.json();
+        setOrdersData(data);
+      } catch (error) {
+        console.error("Error al cargar los datos", error);
+      }
+    };
+    fetchOrders();
+  }, []);
   return (
     <main>
       <NavBar></NavBar>
       <MainLayout>
-        <section className="flex flex-col md:flex-row md:flex-wrap md:justify-between md:gap-4">
-          <section>
-            <KitchenOrderCard bgColor="yellow" time="15">
-              <div className="flex flex-col gap-6">
-                <Order></Order>
-                <Order></Order>
-              </div>
-            </KitchenOrderCard>
-
-            <KitchenOrderCard time="5">
-              <div className="flex flex-col gap-6">
-                <Order></Order>
-                <Order></Order>
-              </div>
-            </KitchenOrderCard>
-          </section>
-          <section>
-            <KitchenOrderCard bgColor="red" time="20">
-              <div className="flex flex-col gap-6">
-                <Order></Order>
-                <Order></Order>
-              </div>
-            </KitchenOrderCard>
-
-            <KitchenOrderCard time="5">
-              <div className="flex flex-col gap-6">
-                <Order></Order>
-                <Order></Order>
-              </div>
-            </KitchenOrderCard>
-          </section>
-
-          <section>
-            <KitchenOrderCard time="5">
-              <div className="flex flex-col gap-6">
-                <Order></Order>
-                <Order></Order>
-              </div>
-            </KitchenOrderCard>
-
-            <KitchenOrderCard time="5">
-              <div className="flex flex-col gap-6">
-                <Order></Order>
-                <Order></Order>
-              </div>
-            </KitchenOrderCard>
-          </section>
-        </section>
+        <div className="flex flex-col gap-4 lg:flex-row">
+          {ordersData.map((statusData, index) => (
+            <StatusOrderCard key={index} status={statusData.status}>
+              {statusData.orders.map((order, idx) => (
+                <div className="mb-3" key={idx}>
+                  <Order
+                    dishName={order.dishName}
+                    table={order.table}
+                    time={order.time}
+                    baseIgredients={order.baseIngredients}
+                    extraIngredients={order.extraIngredients}
+                  />
+                </div>
+              ))}
+            </StatusOrderCard>
+          ))}
+        </div>
       </MainLayout>
     </main>
   );
