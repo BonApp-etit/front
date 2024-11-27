f3c4543678936e034f833d0b0da40d0aaa890584
Leonardo <leonardomp2200@gmail.com>
2024-11-27 11:25:21 -0600

    Change to StatusCard for kitchen orders

---
+++ src/components/kitchen_order/StatusOrderCard.jsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+import LayoutCard from "../common_components/LayoutCard";
+
+export default function StatusOrder({ children, status }) {
+  return (
+    <section className="mb-6 w-full">
+      <div className="lg:max-w-[375px]">
+        <div className="mb-2">
+          <LayoutCard
+            tailwindClasses={clsx(
+              "",
+              { "!bg-cs500": status === "active" },
+              { "!bg-cs600": status === "process" },
+              { "!bg-cs700": status === "done" },
+            )}
+          >
+            <h3 className="text-center font-poppins text-2xl font-semibold text-white md:text-4xl">
+              Activo (1)
+            </h3>
+          </LayoutCard>
+        </div>
+        {children}
+      </div>
+    </section>
+  );
+}
