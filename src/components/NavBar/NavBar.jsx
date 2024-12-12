28001c2df58d6918bd49aab1308d0f7eb7e4505a
Leonardo <leonardomp2200@gmail.com>
2024-12-11 19:55:25 -0600

    Implementation of Avatar button, change of IconBars

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,8 @@ import Logo from "./Logo";
 import Links from "./Links";
 import { useEffect, useState } from "react";
 import clsx from "clsx";
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import Avatar from "../common_components/Avatar";
 
 const NavBar = ({ onToggleCanvas }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -21,13 +22,14 @@ const NavBar = ({ onToggleCanvas }) => {
   ];
 
   return (
-    <nav className="flex justify-center bg-white p-4 shadow-md md:px-14">
+    <nav className="flex justify-center bg-white p-3 shadow-md md:px-14">
       <section className="mb-1 flex w-full max-w-[400px] items-center justify-between gap-1 md:min-w-[632px] md:max-w-[700px] lg:max-w-[1200px]">
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-5">
           {isAuthenticated && (
             <Bars3Icon
               onClick={onToggleCanvas}
-              className="h-6 w-6 text-cs500"
+              className="h-7 w-7 text-cs500"
+              strokeWidth={2}
             />
           )}
           <Logo />
@@ -35,7 +37,7 @@ const NavBar = ({ onToggleCanvas }) => {
 
         <div className="flex items-center gap-4">
           {!isAuthenticated && <Links />}
-          {isAuthenticated ? <h1>AVATAR</h1> : null}
+          {isAuthenticated ? <Avatar /> : null}
 
           <div
             className={clsx(
