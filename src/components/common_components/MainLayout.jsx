5835a59f2c6449140d0cf7de97194fedf0892eba
Leonardo <leonardomp2200@gmail.com>
2024-12-12 15:41:13 -0600

    Adjust of MainLayout

--- src/components/common_components/MainLayout.jsx
+++ src/components/common_components/MainLayout.jsx
@@ -1,6 +1,6 @@
 export default function MainLayout({ children }) {
   return (
-    <section className="flex min-h-dvh justify-center p-4 md:px-14">
+    <section className="flex min-h-dvh justify-center px-3 pb-4 md:px-14">
       <div className="mb-1 flex w-full max-w-[400px] flex-col md:min-w-[632px] md:max-w-[700px] lg:max-w-[1200px]">
         {children}
       </div>
